263111446156dedc1987ae60150d9523b050d6a8
StegmayerTobias <tobiassebastianstegmayer@gmail.com>
2025-06-14 22:07:31 -0300

    funcion salida

--- buscar_menor_en_fila.py
+++ buscar_menor_en_fila.py
@@ -1,13 +1,13 @@
-def buscar_menor(tps:list) -> list:
+def buscar_menor(fila:list) -> int:
     """
-    1. Esta función recibe una lista de tiempos de proxima salida (TPS) y devuelve el menor tiempo
-    2. Esta función recibe una lista de cantidad de personas por servidor (NS) y devuelve la de menor cantidad.
+    1. Esta función recibe una lista de tiempos de proxima salida (fila) y devuelve la posicion del menor tiempo
+    2. Esta función recibe una lista de cantidad de personas por servidor (NS) y devuelve la posicion de menor cantidad.
     """
-    menor = tps[0]
+    menor = fila[0]
     pos = 0
-    for i in range(1, len(tps)): 
-        if tps[i] < menor:
-            menor = tps[i]
+    for i in range(1, len(fila)): 
+        if fila[i] < menor:
+            menor = fila[i]
             pos = i
     print("El menor en la fila es:", menor, "en la posición", pos)
-    return [pos, menor]
+    return pos
--- salida.py
+++ salida.py
@@ -1,5 +1,28 @@
-def salida(STP, TPSk, T, NS, CTO):
+from buscar_menor_en_fila import buscar_menor
+from generar_ta import generar_tiempo_de_atencion
+
+def salida(STP:list, TPS:list, k:list, T:int, NS:list,STA:list, CTO:list):
     """
     Analiza todo el lado derecho del diagrama, es decir, para el caso de que el tiempo de llegada sea mayor
     que el tiempo de salida, entonces se producira una salida del sistema.
     """
+
+    for i in range(len(STP)):
+      # sumatoria de tiempo de permanencia en el subsistema i
+      STP[i] += TPS[k] - T * NS[i]
+    
+    T = TPS[k]
+    NS[k] -= 1
+
+    if NS[k] >= 1:
+    # ya hay personas en el subsistema k
+      TA = generar_tiempo_de_atencion()
+      TPS[k] = T + TA
+      STA[k]+= TA
+    else:
+      # fila vacia, comienzo de tiempo ocioso de subsistema k
+      CTO[k] = T
+      TPS[k] = 10000000
+
+
+    
