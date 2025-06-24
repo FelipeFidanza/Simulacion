12e08d51a502f1448980e26631f55f25154625d5
StegmayerTobias <tobiassebastianstegmayer@mail.com>
2025-06-23 21:56:50 -0300

    Agregue parte del flujo de control del algoritmo

--- models/Simulacion.py
+++ models/Simulacion.py
@@ -1,13 +1,38 @@
+from LectorCSV import LectorCSV
+from Sistema import Sistema
+from Subsistema import Subsistema
+
+
 class Simulacion:
     def __init__(
         self,
         cant_corridas,
+        cant_servidores
     ):
         self.cant_corridas = cant_corridas
-    
-    def iniciar_corrida(self):
-        pass
+        self.cant_servidores = cant_servidores
+
+    def iniciar_corrida(self, datos_x_corrida):
+        sistema = Sistema(subsistemas=[])
+        subsistemas = [Subsistema(sistema)
+                       for _ in range(self.cant_servidores)]
+        sistema.subsistemas = subsistemas
+
+        for cliente in datos_x_corrida:
+            [intervalo, tiempo_atencion] = lector.obtener_siguiente()
+            sistema.obtener_proxima_llegada(intervalo)
+            subsistema_prox_salida = sistema.obtener_proxima_salida()
+
+            if sistema.tiempo_proxima_llegada <= sistema.tiempo_proxima_salida:
+                sistema.arribar_cliente(
+                    tiempo_atencion, sistema.tiempo_proxima_llegada)
+            else:
+                subsistema_prox_salida.finalizar_atencion()
 
     def iniciar_simulacion(self):
+        datos_x_corrida = int(lector.intervalos_arribo/self.cant_corridas)
         for corrida in range(1, self.cant_corridas + 1):
-            self.iniciar_corrida()
+            self.iniciar_corrida(datos_x_corrida)
+
+
+lector = LectorCSV('variables.csv')
--- models/Sistema.py
+++ models/Sistema.py
@@ -3,6 +3,7 @@ import math
 from Cliente import Cliente
 from Subsistema import Subsistema
 
+
 class Sistema:
     """
     Clase que representa el sistema de atención al cliente, 
@@ -12,11 +13,11 @@ class Sistema:
     """
 
     def __init__(
-        self, 
-        subsistemas,  
-        tiempo_final = 14400,
-        tiempo_arrepentimiento = 300,
-    ): 
+        self,
+        subsistemas,
+        tiempo_final=14400,
+        tiempo_arrepentimiento=300,
+    ):
         self.subsistemas = subsistemas
         self.tiempo = 0
         self.tiempo_proxima_llegada = 0
@@ -27,65 +28,37 @@ class Sistema:
         self.cant_arrepentidos = 0
         self.tiempo_final = tiempo_final
 
-
-    def generar_ia(self):
-        """Genera un intervalo entre arribos de clientes al sistema con la inversa de la distribución exponencial"""
-        #TODO : tiene que leer un csv con los nros aleatorios
-        U = random.uniform(0, 1)
-        return -math.log(U) / self.lamda
-    
-    def generar_ta(self):
-        """Genera un tiempo de atención para un cliente con la inversa de la distribución exponencial"""
-        #TODO : tiene que leer un csv con los nros aleatorios
-        U = random.uniform(0, 1)
-        return -math.log(U) / self.mu
-    
     def buscar_fila_mas_corta(self) -> Subsistema:
-        #TODO: incializar la variable de tal forma que nunca cambiemos el tipo int -> objeto Subsistema
+        # TODO: incializar la variable de tal forma que nunca cambiemos el tipo int -> objeto Subsistema
         subsistema_menor_fila = self.subsistemas[0]
-        for subsistema in self.subsistemas: 
+        for subsistema in self.subsistemas:
             if len(subsistema.clientes) < len(subsistema_menor_fila.clientes):
                 subsistema_menor_fila = subsistema
         return subsistema_menor_fila
-    
-    def obtener_proxima_llegada(self):
-        intervalo_entre_arribos = self.generar_ia()
+
+    def obtener_proxima_llegada(self, intervalo_entre_arribos):
         self.tiempo_proxima_llegada = self.tiempo + intervalo_entre_arribos
-       
-    def obtener_proxima_salida(self):
+
+    def obtener_proxima_salida(self) -> Subsistema:
         subsistema_proxima_salida = self.subsistemas[0]
-        for subsistema in self.subsistemas: 
+        for subsistema in self.subsistemas:
             if subsistema.tiempo_proxima_salida < subsistema_proxima_salida.tiempo_proxima_salida:
                 subsistema_proxima_salida = subsistema
         self.tiempo_proxima_salida = subsistema_proxima_salida.tiempo_proxima_salida
         return subsistema_proxima_salida
-    
-    
 
-    
-    # Para nosotros que esto vaya en el flujo nomas :) 
-
-    # def verificar_proximo_evento(self):
-    #     """
-    #     Verifica cuál es el próximo evento a ocurrir en el sistema: una llegada o una salida.
-    #     """
-    #     self.obtener_proxima_llegada()
-    #     self.obtener_proxima_salida()
-
-    #     return self.tiempo_proxima_llegada <= self.tiempo_proxima_salida
-    
-    def arribar_cliente(self):
+    def arribar_cliente(self, tiempo_atencion, tiempo_prox_llegada):
         """
         Genera un cliente con un tiempo de llegada y un tiempo de atención.
         El tiempo de llegada es el tiempo actual del sistema y el tiempo de atención es generado aleatoriamente.
         """
-        tiempo_llegada = self.tiempo
-        tiempo_atencion = self.generar_ta()
-        cliente = Cliente(tiempo_llegada, tiempo_atencion)
+        cliente = Cliente(tiempo_prox_llegada, tiempo_atencion)
 
         fila_a_ingresar = self.buscar_fila_mas_corta()
 
         fila_a_ingresar.recibir_cliente(cliente)
-        
+
+        self.tiempo = tiempo_prox_llegada
+
     # def verificar_tiempo_final(self):
     #     return self.tiempo_final <= self.tiempo
